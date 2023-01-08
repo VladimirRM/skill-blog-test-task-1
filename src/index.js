3bf5c56c039dac3006076e5d8ec842a6388c0b98
Vladimir RM <shvdrummer@gmail.com>
2023-01-08 19:40:40 +0200

    adding provider

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ const store = createStore( rootReducer,
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
